a46ad3750621df519f05809d1db60fe52e44835a
Ruben <r.yildiz@student.helmo.be>
2024-05-10 18:16:12 +0200

    colormap

--- src/color/MapColoring.java
+++ src/color/MapColoring.java
@@ -2,20 +2,16 @@ package color;
 
 import java.awt.Color;
 import java.awt.image.BufferedImage;
-import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.logging.Logger;
 
-import org.jgrapht.Graph;
-import org.jgrapht.Graphs;
-import org.jgrapht.alg.interfaces.VertexColoringAlgorithm;
+import connectivity.UndirectedConnectivityInspector;
 import org.jgrapht.alg.interfaces.VertexColoringAlgorithm.Coloring;
 import org.jgrapht.graph.DefaultEdge;
 import org.jgrapht.graph.SimpleGraph;
-import connectivity.UndirectedConnectivityInspector;
 
 /**
  * Coloring of a Map image.
@@ -24,12 +20,18 @@ import connectivity.UndirectedConnectivityInspector;
  *
  */
 public class MapColoring {
+	private static final Logger logger = Logger.getLogger(MapColoring.class.getName());
 
 	// Tableau de couleurs prédéfini
 	@SuppressWarnings("unused")
 	private static final int[] rgbColors = { Color.BLUE.getRGB(), Color.RED.getRGB(), Color.GREEN.getRGB(),
 			Color.YELLOW.getRGB(), Color.CYAN.getRGB(), Color.PINK.getRGB(), Color.ORANGE.getRGB(),
 			Color.MAGENTA.getRGB() };
+
+	private final BufferedImage image;
+	private final Color borderColor;
+	private SimpleGraph<Integer, DefaultEdge> zoneAdjacencyGraph;
+	private List<Set<Integer>> zones;
 
 	/**
 	 * Constructor
@@ -39,7 +41,11 @@ public class MapColoring {
 	 * @throws NullPointerException if any input parameter is null
 	 */
 	public MapColoring(BufferedImage image, Color borderColor) {
-		//todo
+		if (image == null || borderColor == null){
+			throw new NullPointerException("Input image or border color cannot be null.");
+		}
+		this.image = image;
+		this.borderColor = borderColor;
 	}
 
 	/**
@@ -47,17 +53,83 @@ public class MapColoring {
 	 * have different colors.
 	 */
 	public void colorMap() {
-		//todo
+		// Obtenez la coloration du graphe d'adjacence
+		Coloring<Integer> coloring = getColoring();
+
+		if (coloring != null) {
+			Map<Integer, Integer> zoneColors = coloring.getColors();
+
+			// Récupérez les ensembles de zones identifiés
+			zones = identifyZone();
+
+			// Appliquez les couleurs appropriées à chaque point dans chaque zone
+			for (int zoneIndex = 0; zoneIndex < zones.size(); zoneIndex++) {
+				Set<Integer> zonePoints = zones.get(zoneIndex);
+				int colorIndex = zoneColors.get(zoneIndex);
+				int rgbColor = rgbColors[colorIndex % rgbColors.length]; // Évitez un dépassement d'index
+
+				// Coloriez chaque point de la zone
+				for (int pointIndex : zonePoints) {
+					int x = pointIndex % image.getWidth();
+					int y = pointIndex / image.getWidth();
+					image.setRGB(x, y, rgbColor);
+				}
+			}
+
+			logger.info("Carte colorée avec succès !");
+		}
 	}
 
 	/**
 	 * Return the adjacency graph of the image's areas.
-	 * 
+	 *
 	 * @return the adjacency graph of the image's areas
 	 */
 	public SimpleGraph<Integer, DefaultEdge> adjacencyGraph() {
-		return null; //todo
+		SimpleGraph<Integer, DefaultEdge> adjacencyGraph = new SimpleGraph<>(DefaultEdge.class);
+		Map<Integer, Integer> pointToVertex = new HashMap<>();
+		int width = image.getWidth();
+		int height = image.getHeight();
+		int vertexId = 0;
+
+		// Créez des sommets pour chaque pixel non-bordure
+		for (int y = 0; y < height; y++) {
+			for (int x = 0; x < width; x++) {
+				int rgb = image.getRGB(x, y);
+				if (rgb != borderColor.getRGB()) {
+					int index = y * width + x;
+					pointToVertex.put(index, vertexId);
+					adjacencyGraph.addVertex(vertexId);
+					vertexId++;
+				}
+			}
+		}
+
+		// Créez les arêtes entre les pixels voisins non-bordure
+		int[][] neighborOffsets = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+		for (Map.Entry<Integer, Integer> entry : pointToVertex.entrySet()) {
+			int index = entry.getKey();
+			int vertex = entry.getValue();
+			int x = index % width;
+			int y = index / width;
+
+			for (int[] offset : neighborOffsets) {
+				int neighborX = x + offset[0];
+				int neighborY = y + offset[1];
+				if (neighborX >= 0 && neighborX < width && neighborY >= 0 && neighborY < height) {
+					int neighborIndex = neighborY * width + neighborX;
+					if (pointToVertex.containsKey(neighborIndex)) {
+						int neighborVertex = pointToVertex.get(neighborIndex);
+						if (!adjacencyGraph.containsEdge(vertex, neighborVertex)) {
+							adjacencyGraph.addEdge(vertex, neighborVertex);
+						}
+					}
+				}
+			}
+		}
+		return adjacencyGraph;
 	}
+
 
 	/**
 	 * Return the coloring of the adjacency graph.
@@ -65,7 +137,101 @@ public class MapColoring {
 	 * @return the coloring of the adjacency graph
 	 */
 	public Coloring<Integer> getColoring() {
-		return null; //todo
+		SimpleGraph<Integer, DefaultEdge> graph = createZoneAdjacencyGraph();
+		DSaturColoring<Integer, DefaultEdge> dsatur = new DSaturColoring<>(graph);
+		return dsatur.getColoring();
+	}
+
+
+	public List<Set<Integer>> identifyZone() {
+		// Utilisez un graphe d'adjacence pour identifier les zones.
+		SimpleGraph<Integer, DefaultEdge> adjacencyGraph = adjacencyGraph();
+		return new UndirectedConnectivityInspector<>(adjacencyGraph).connectedSets();
+	}
+
+	public SimpleGraph<Integer, DefaultEdge> createZoneAdjacencyGraph() {
+		if (zoneAdjacencyGraph == null) {
+			zoneAdjacencyGraph = new SimpleGraph<>(DefaultEdge.class);
+			zones = identifyZone();
+			logger.info(String.format("Identified %d zones for the adjacency graph.", zones.size()));
+
+			// Add vertices
+			for (int i = 0; i < zones.size(); i++) {
+				zoneAdjacencyGraph.addVertex(i);
+				logger.info(String.format("Vertex %s added to the adjacency graph.", i));
+			}
+
+			// Add edges only if two zones share a border
+			for (int i = 0; i < zones.size(); i++) {
+				Set<Integer> zoneA = zones.get(i);
+				String zoneAName = "Z" + i;
+
+				for (int j = i + 1 ; j < zones.size(); j++) {
+					Set<Integer> zoneB = zones.get(j);
+					String zoneBName = "Z" + j;
+
+					if (hasSharedBorder(zoneA, zoneB)) {
+						zoneAdjacencyGraph.addEdge(i, j);
+						logger.info(String.format("Edge added between %s and %s.", zoneAName, zoneBName));
+					} else {
+						logger.info(String.format("No shared border between %s and %s.", zoneAName, zoneBName));
+					}
+				}
+			}
+
+			logger.info(String.format("Zone adjacency graph created with %d vertices and %d edges.",
+					zoneAdjacencyGraph.vertexSet().size(), zoneAdjacencyGraph.edgeSet().size()));
+		}
+		return zoneAdjacencyGraph;
+	}
+
+
+	private boolean hasSharedBorder(Set<Integer> zoneA, Set<Integer> zoneB) {
+		int width = image.getWidth();
+		int[] horizontalOffsets = {-1, 1};
+		int[] verticalOffsets = {-width, width};
+
+		for (int pointA : zoneA) {
+			int xA = pointA % width;
+			int yA = pointA / width;
+
+			// Vérifiez les voisins horizontaux (gauche/droite)
+			for (int hOffset : horizontalOffsets) {
+				for (int distance = 1; distance <= 4; distance++) {
+					int neighbor = pointA + hOffset * distance;
+					int xNeighbor = neighbor % width;
+					int yNeighbor = neighbor / width;
+
+					// Vérifiez si le voisin est sur la même ligne
+					if (zoneB.contains(neighbor) && yA == yNeighbor) {
+						logger.info(String.format(
+								"Horizontal adjacency: Point %d (%d, %d) in ZoneA is adjacent to Point %d (%d, %d) in ZoneB.",
+								pointA, xA, yA, neighbor, xNeighbor, yNeighbor));
+						return true;
+					}
+				}
+			}
+
+			// Vérifiez les voisins verticaux (haut/bas)
+			for (int vOffset : verticalOffsets) {
+				for (int distance = 1; distance <= 4; distance++) {
+					int neighbor = pointA + vOffset * distance;
+					int xNeighbor = neighbor % width;
+					int yNeighbor = neighbor / width;
+
+					// Vérifiez si le voisin est dans la même colonne
+					if (zoneB.contains(neighbor) && xA == xNeighbor) {
+						logger.info(String.format(
+								"Vertical adjacency: Point %d (%d, %d) in ZoneA is adjacent to Point %d (%d, %d) in ZoneB.",
+								pointA, xA, yA, neighbor, xNeighbor, yNeighbor));
+						return true;
+					}
+				}
+			}
+		}
+
+		logger.info("No shared border found between zones.");
+		return false;
 	}
 
 }
--- test/color/MapColoringTest.java
+++ test/color/MapColoringTest.java
@@ -4,6 +4,11 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.awt.Color;
 import java.awt.image.BufferedImage;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.logging.Logger;
 
 import javax.imageio.ImageIO;
 
@@ -16,6 +21,7 @@ import graphics.Image;
 
 public class MapColoringTest {
 
+	private static final Logger logger = Logger.getLogger(MapColoringTest.class.getName());
 	@Test
 	void nullImage() {
 		MapColoring mc = null;
@@ -57,41 +63,163 @@ public class MapColoringTest {
 		assertNotNull(coloring);
 		assertEquals(4, coloring.getNumberColors());
 	}
-	
+
 	@Test
-    void mediumMapColoring() {
-        BufferedImage image = Image.loadImage("img/maps/three_colors_tiling.png");
+	void testAdjacencyGraphSimpleImage() {
+		// Create a simple test image
+		int width = 5, height = 5;
+		BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
 
-        MapColoring mc = new MapColoring(image, Color.BLACK);
-        assertNotNull(mc);
-        mc.colorMap();
+		// Fill the image with all black (border color)
+		Color borderColor = Color.BLACK;
+		for (int y = 0; y < height; y++) {
+			for (int x = 0; x < width; x++) {
+				image.setRGB(x, y, borderColor.getRGB());
+			}
+		}
 
-        SimpleGraph<Integer, DefaultEdge> graph = mc.adjacencyGraph();
-        assertNotNull(graph);
-        assertEquals(25, graph.vertexSet().size());
-        assertEquals(52, graph.edgeSet().size());
+		// Draw a white square in the middle
+		Color zoneColor = Color.WHITE;
+		image.setRGB(2, 1, zoneColor.getRGB());
+		image.setRGB(2, 2, zoneColor.getRGB());
+		image.setRGB(1, 2, zoneColor.getRGB());
+		image.setRGB(3, 2, zoneColor.getRGB());
+		image.setRGB(2, 3, zoneColor.getRGB());
 
-        VertexColoringAlgorithm.Coloring<Integer> coloring = mc.getColoring();
-        assertNotNull(coloring);
-        assertEquals(5, coloring.getNumberColors());
-    }
+		// Initialize MapColoring with the created image
+		MapColoring mapColoring = new MapColoring(image, borderColor);
+		SimpleGraph<Integer, DefaultEdge> graph = mapColoring.adjacencyGraph();
 
-    @Test
-    void largeMapColoring() {
-        BufferedImage image = Image.loadImage("img/maps/conformally-regular-pentagonal-tiling.png");
+		// Check if the graph contains the expected number of vertices (5)
+		assertEquals(5, graph.vertexSet().size(), "Graph should contain 5 vertices");
 
-        MapColoring mc = new MapColoring(image, Color.BLACK);
-        assertNotNull(mc);
-        mc.colorMap();
+		// Check if the graph contains the expected number of edges (4)
+		assertEquals(4, graph.edgeSet().size(), "Graph should contain 4 edges");
 
-        SimpleGraph<Integer, DefaultEdge> graph = mc.adjacencyGraph();
-        assertNotNull(graph);
-        assertEquals(50, graph.vertexSet().size());
-        assertEquals(122, graph.edgeSet().size());
+		// Check if specific edges are present (verify adjacency)
+		assertTrue(graph.containsEdge(0, 2) || graph.containsEdge(1, 0), "Edge between vertices 0 and 1 should exist");
+		assertTrue(graph.containsEdge(1, 2) || graph.containsEdge(2, 0), "Edge between vertices 0 and 2 should exist");
+		assertTrue(graph.containsEdge(2, 3) || graph.containsEdge(3, 0), "Edge between vertices 0 and 3 should exist");
+		assertTrue(graph.containsEdge(2, 4) || graph.containsEdge(4, 0), "Edge between vertices 0 and 4 should exist");
+	}
 
-        VertexColoringAlgorithm.Coloring<Integer> coloring = mc.getColoring();
-        assertNotNull(coloring);
-        assertEquals(7, coloring.getNumberColors());
-    }
+	@Test
+	void testIdentifyZonesSimpleImage() {
+		// Create a simple test image with multiple zones
+		int width = 5, height = 5;
+		BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
 
+		// Fill the entire image with border color (black)
+		Color borderColor = Color.BLACK;
+		for (int y = 0; y < height; y++) {
+			for (int x = 0; x < width; x++) {
+				image.setRGB(x, y, borderColor.getRGB());
+			}
+		}
+
+		// Create a zone (Z1) in the top-left corner
+		Color zoneColor1 = Color.WHITE;
+		image.setRGB(0, 0, zoneColor1.getRGB());
+		image.setRGB(1, 0, zoneColor1.getRGB());
+		image.setRGB(0, 1, zoneColor1.getRGB());
+		image.setRGB(1, 1, zoneColor1.getRGB());
+
+		// Create a separate zone (Z2) in the bottom-right corner
+		Color zoneColor2 = Color.GREEN;
+		image.setRGB(3, 3, zoneColor2.getRGB());
+		image.setRGB(4, 3, zoneColor2.getRGB());
+		image.setRGB(3, 4, zoneColor2.getRGB());
+		image.setRGB(4, 4, zoneColor2.getRGB());
+
+		// Initialize MapColoring with the created image
+		MapColoring mapColoring = new MapColoring(image, borderColor);
+		List<Set<Integer>> zones = mapColoring.identifyZone();
+
+		// Check the number of identified zones
+		assertEquals(2, zones.size(), "The graph should contain 2 separate zones.");
+
+		// Verify that each zone contains the expected number of vertices
+		boolean foundZone1 = false;
+		boolean foundZone2 = false;
+		logger.info(String.format("ZoneS : %s", zones));
+		for (Set<Integer> zone : zones) {
+			logger.info(String.format("Found zone size: %d", zone.size()));
+			if (zone.size() == 4) {
+				logger.info(String.format("Found zone : %s", zone));
+				if (zone.contains(0) && zone.contains(1) && zone.contains(2) && zone.contains(3)) {
+					foundZone1 = true;
+				} else if (zone.contains(4) && zone.contains(5) && zone.contains(6) && zone.contains(7)) {
+					foundZone2 = true;
+				}
+			}
+		}
+
+		assertTrue(foundZone1, "Zone 1 should be identified correctly.");
+		assertTrue(foundZone2, "Zone 2 should be identified correctly.");
+	}
+
+	@Test
+	void testCreateZoneAdjacencyGraph() {
+		// Crée une image de test
+		int width = 7, height = 7;
+		BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+
+		// Remplit l'image avec une couleur de bordure (noir)
+		Color borderColor = Color.BLACK;
+		for (int y = 0; y < height; y++) {
+			for (int x = 0; x < width; x++) {
+				image.setRGB(x, y, borderColor.getRGB());
+			}
+		}
+
+		// Dessine plusieurs zones distinctes pour les tests
+		// Zone 0 : rectangle blanc 3x3 en haut à gauche
+		Color zoneColor1 = Color.WHITE;
+		for (int y = 1; y <= 3; y++) {
+			for (int x = 1; x <= 3; x++) {
+				image.setRGB(x, y, zoneColor1.getRGB());
+			}
+		}
+
+		// Zone 1 : rectangle cyan 2x3 en haut à droite
+		Color zoneColor2 = Color.CYAN;
+		for (int y = 1; y <= 3; y++) {
+			for (int x = 5; x <= 6; x++) {
+				image.setRGB(x, y, zoneColor2.getRGB());
+			}
+		}
+
+		// Zone 2 : rectangle vert 2x2 en bas
+		Color zoneColor3 = Color.GREEN;
+		for (int y = 5; y <= 6; y++) {
+			for (int x = 3; x <= 4; x++) {
+				image.setRGB(x, y, zoneColor3.getRGB());
+			}
+		}
+
+		// Zone 3 : rectangle jaune 2x1 adjacent à la Zone 2
+		Color zoneColor4 = Color.YELLOW;
+		image.setRGB(6, 5, zoneColor4.getRGB());
+		image.setRGB(6, 6, zoneColor4.getRGB());
+
+		// Crée une instance de MapColoring
+		MapColoring mapColoring = new MapColoring(image, borderColor);
+
+		// Crée le graphe d'adjacence des zones
+		SimpleGraph<Integer, DefaultEdge> graph = mapColoring.createZoneAdjacencyGraph();
+
+		// Vérifie que le graphe contient les sommets attendus (indices de 0 à 3)
+		Set<Integer> expectedVertices = Set.of(0, 1, 2, 3);
+		assertEquals(expectedVertices, graph.vertexSet(), "Le graphe doit contenir les sommets 0, 1, 2 et 3");
+
+		// Vérifie les arêtes entre les zones adjacentes
+		assertTrue(graph.containsEdge(2, 3) || graph.containsEdge(3, 2), "Les zones 2 et 3 doivent être adjacentes");
+
+		// Vérifie que les zones non adjacentes ne sont pas connectées
+		assertFalse(graph.containsEdge(0, 1), "Les zones 0 et 1 ne doivent pas être adjacentes");
+		assertFalse(graph.containsEdge(0, 2), "Les zones 0 et 2 ne doivent pas être adjacentes");
+		assertFalse(graph.containsEdge(0, 3), "Les zones 0 et 3 ne doivent pas être adjacentes");
+		assertFalse(graph.containsEdge(1, 2), "Les zones 1 et 2 ne doivent pas être adjacentes");
+		assertFalse(graph.containsEdge(1, 3), "Les zones 1 et 3 ne doivent pas être adjacentes");
+	}
 }
